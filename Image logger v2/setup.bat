605cc88f397216a6abec73da8adb51cafa255960
VsyncwaBeams <118215980+VsyncwaBeams@users.noreply.github.com>
2022-12-05 21:03:36 +0100

    Add files via upload

---
+++ Image logger v2/setup.bat
@@ -0,0 +1,9 @@
+echo Installing Modules..
+pip install os
+pip install discord_webhook
+pip install time
+pip install colorama
+pip install discord
+pip install requests
+pip install sys
+echo All Modules have been downloaded..
